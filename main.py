5af29881b39fe03ac1b1a154c1b1d27bc7921f51
SHRIJEET <shrijeetj@gmail.com>
2019-09-30 21:09:04 -0400

    epsilon-Differential privacy implementation

--- main.py
+++ main.py
@@ -6,59 +6,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-plt.style.use('seaborn-deep')
-
 
 emp_df = pd.read_csv('IL_employee_salary.csv', keep_default_na=True, skip_blank_lines=True).dropna();
-emp_sal = emp_df['Annual Salary'].str.strip();
-emp_sal = emp_sal.str.replace('$', '')
-emp_sal = emp_sal.str.replace(',', '')
-emp_sal = np.array(emp_sal.astype(int))
-
-bins = np.array([50000, 60000, 70000, 80000, 90000, 100000])
-
-highest_sal = 0
-second_highest_sal = 0
-for sal in emp_sal:
-    if highest_sal < sal:
-        second_highest_sal = highest_sal
-        highest_sal = sal
-
-np.random.seed(1576436)
-
-_EPSILON = 0.05
-lmda = float(highest_sal - second_highest_sal)/float(_EPSILON)
-noise = np.random.laplace(scale=lmda, size=len(emp_sal))
-new_vals1 = []
-for i, n in enumerate(noise):
-    new_vals1.append(emp_sal[i] + int(abs(noise[i])))
-
-_EPSILON = 0.1
-lmda = float(highest_sal - second_highest_sal)/float(_EPSILON)
-noise = np.random.laplace(scale=lmda, size=len(emp_sal))
-new_vals2 = []
-for i, n in enumerate(noise):
-    new_vals2.append(emp_sal[i] + int(abs(noise[i])))
-
-_EPSILON = 5.0
-lmda = float(highest_sal - second_highest_sal)/float(_EPSILON)
-noise = np.random.laplace(scale=lmda, size=len(emp_sal))
-new_vals3 = []
-for i, n in enumerate(noise):
-    new_vals3.append(emp_sal[i] + int(abs(noise[i])))
-
-plt.hist([emp_sal, new_vals1, new_vals2, new_vals3], bins, label=['None', '0.05','0.1','5.0'])
-plt.ylabel('Number of Employees')
-plt.xlabel('Salary Brackets')
-plt.legend(prop={'size': 10})
+emp_df['Annual Salary'] = emp_df['Annual Salary'].str.replace(',', '')
+emp_df['Annual Salary'] = emp_df['Annual Salary'].str.replace('$', '')
+emp_df['Annual Salary'] = emp_df['Annual Salary'].astype(float)
+bins = [50000, 60000, 70000, 80000, 90000, 100000]
+count, bins, bars = plt.hist(emp_df['Annual Salary'], bins=bins)
 plt.show()
 
+#np.random.seed(1576436)
+sensitivity = 1
+epsilon1 = 0.05
+epsilon2 = 0.1
+epsilon3 = 5.0
 
+lmda1 = sensitivity/epsilon1
+lmda2 = sensitivity/epsilon2
+lmda3 = sensitivity/epsilon3
 
+noise1 = np.random.laplace(0., lmda1, len(bins)-1)
+noise2 = np.random.laplace(0., lmda2, len(bins)-1)
+noise3 = np.random.laplace(0., lmda3, len(bins)-1)
 
+data = np.arange(len(count))
+data1 = [x + 0.2 for x in data]
+data2 = [x + 0.2 for x in data1]
+data3 = [x + 0.2 for x in data2]
 
-
-
-
-
-
+plt.bar(data, count, color='#eb4034', width=0.2, edgecolor='white', label='NA')
+plt.bar(data1, count + noise1, color='#ebe534', width=0.2, edgecolor='white', label='0.05')
+plt.bar(data2, count + noise2, color='#43eb34', width=0.2, edgecolor='white', label='0.1')
+plt.bar(data3, count + noise3, color='#3440eb', width=0.2, edgecolor='white', label='5.0')
+plt.legend()
+plt.show()
